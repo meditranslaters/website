83a0df85f8acaadc21ca89742228c1449539e347
xming13 <tanweiseng13@gmail.com>
2020-05-04 03:45:37 +0800

    refactor: clean up readSheet

--- src/Methods/readSheet.js
+++ src/Methods/readSheet.js
@@ -1,47 +1,45 @@
-// import the library
-import excel from 'xlsx';
-import axios from 'axios';
 import {props__export} from '../App.js'
 import data from './data.js'
 
-const website_url = "http://localhost:8080"
+export function getSupportedLanguages() {
+  const languagesSupported = [
+    "English",
+    ...data.map(d => d[2]),
+  ];
 
-export function getSupportedLanguages(){
-  var rows = data
-  var languages_supported = ["English"]
-  for(var i = 1 ; i < rows.length ; i++){
-      languages_supported.push(rows[i][2])
-  }
-  //converting to a set
-  var mySet = new Set(languages_supported)
-  var arr = Array.from(mySet)
+  // De-duplicate the language
+  // return Array.from(new Set(languagesSupported));
+  const arr = Array.from(new Set(languagesSupported));
+
   props__export.setState({supported_langs:[]})
-  for( var i = 0 ; i < arr.length; i++){
+
+  for( var i = 0 ; i < arr.length; i++) {
     props__export.setState({supported_langs:props__export.state.supported_langs.push(arr[i])})
   }
-
-
 }
 
-export function getLanguageData(lang_a,lang_b){
+export function getLanguageData(lang_a, lang_b) {
+  const lang_data = readLanguageData(lang_a, lang_b)
 
-    var lang_data = readLanguageData(lang_a,lang_b,data)
+  // return {
+  //   a_languagedata: lang_data["a_languagedata"],
+  //   a_language: lang_a,
+  //   b_languagedata: lang_data["b_languagedata"],
+  //   b_language: lang_b,
+  //   categories: lang_data["categories"],
+  // }
 
-
-
-    setTimeout(
-      function() {
-        props__export.setState({a_languagedata:lang_data["a_languagedata"]})
-        props__export.setState({a_language:lang_a})
-        props__export.setState({b_languagedata:lang_data["b_languagedata"]})
-        props__export.setState({b_language:lang_b})
-        props__export.setState({categories:lang_data["categories"]})
-      }
-      .bind(this),
-      100
-    );
-
-
+  setTimeout(
+    function() {
+      props__export.setState({a_languagedata:lang_data["a_languagedata"]})
+      props__export.setState({a_language:lang_a})
+      props__export.setState({b_languagedata:lang_data["b_languagedata"]})
+      props__export.setState({b_language:lang_b})
+      props__export.setState({categories:lang_data["categories"]})
+    }
+    .bind(this),
+    100
+  );
 }
 
 
@@ -53,72 +51,53 @@ export function onChangeSearch(e){
   this.setState({search_input:e.target.value})
 }
 
-function readLanguageData(language_a,language_b,rows){
-  var language_from_rows = []
-  var language_to_rows = []
-  var language_a_rows = []
-  var rows_sort = []
+function readLanguageData(language_a,language_b) {
+  let rows = data;
+  const response_json = {
+    "a_languagedata": [],
+    "b_languagedata": [],
+    "categories": []
+  };
 
-  var response_json = {"a_languagedata":[],"b_languagedata":[],"categories":[]}
-  if(language_a == "English" && language_b == "English"){
-    return response_json
-  }else if(language_a == "English" && language_b != "English"){
-    //get the translations for language b
-    //seperate them for english as langauge a and language b
-    var language_a_data = []
-    var language_b_data = []
+  if (language_a === "English" && language_b === "English") {
+    return response_json;
+  }
 
+  if (language_a === "English" && language_b !== "English") {
+    const filteredData = data.filter(item => item[2] === language_b);
+    const language_a_data = filteredData.map(item => [item[0], item[3]]);
+    const language_b_data = filteredData.map(item => [item[1], item[3]]);
 
-    for(var i = 0 ; i < rows.length ; i++){
-          // console.log('language_b',language_b)
-          // console.log('language_b',rows[i][2])
-          if(rows[i][2] == language_b){
+    const categories = [
+      "All",
+      ...filteredData.map(item => item[3]),
+    ];
 
-            language_a_data.push([rows[i][0],rows[i][3]])
-            language_b_data.push([rows[i][1],rows[i][3]])
-            rows_sort.push(rows[i])
-          }
+    return {
+      a_languagedata: language_a_data.map((item, index) => ({ ...item, number: index + 1 })),
+      b_languagedata: language_b_data.map((item, index) => ({ ...item, number: index + 1 })),
+      categories,
     }
+  }
 
-    var categories = ["All"]
-    for(var i = 0 ; i < rows_sort.length ; i++){
-          categories.push(rows_sort[i][3])
+  if (language_a !== "English" && language_b === "English") {
+    const filteredData = data.filter(item => item[2] === language_a);
+    const language_a_data = filteredData.map(item => [item[1], item[3]]);
+    const language_b_data = filteredData.map(item => [item[0], item[3]]);
+
+    const categories = [
+      "All",
+      ...filteredData.map(item => item[3]),
+    ];
+
+    return {
+      a_languagedata: language_a_data.map((item, index) => ({ ...item, number: index + 1 })),
+      b_languagedata: language_b_data.map((item, index) => ({ ...item, number: index + 1 })),
+      categories,
     }
-    response_json["a_languagedata"] = language_a_data.map((item, index) => ({ ...item, number: index + 1 }))
-    response_json["b_languagedata"] = language_b_data.map((item, index) => ({ ...item, number: index + 1 }))
-    response_json["categories"] = categories
-    return response_json
+  }
 
-
-
-  }else if(language_a != "English" && language_b =="English"){
-    var language_a_data = []
-    var language_b_data = []
-    //get the translations for language a
-    //seperate them for language a and english as langauge b
-
-    for(var i = 0 ; i < rows.length ; i++){
-
-          if(rows[i][2] == language_a){
-            language_b_data.push([rows[i][0],rows[i][3]])
-            language_a_data.push([rows[i][1],rows[i][3]])
-            rows_sort.push(rows[i])
-          }
-
-    }
-    var categories = ["All"]
-    for(var i = 0 ; i < rows_sort.length ; i++){
-          categories.push(rows_sort[i][3])
-    }
-    response_json["a_languagedata"] = language_a_data.map((item, index) => ({ ...item, number: index + 1 }))
-    response_json["b_languagedata"] = language_b_data.map((item, index) => ({ ...item, number: index + 1 }))
-    response_json["categories"] = categories
-    return response_json
-
-
-
-    // res.send(response_json)
-  }else if(language_a != "English" && language_b != "English"){
+  if (language_a !== "English" && language_b !== "English") {
     //get the translations for language a,b
     //seperate them for language a and language a and b
     var language_a_data = []
@@ -158,15 +137,13 @@ function readLanguageData(language_a,language_b,rows){
           language_b_data.push(lang_dict[i][language_b])
         }
 
-   }
+    }
 
-  response_json["a_languagedata"] = language_a_data.map((item, index) => ({ ...item, number: index + 1 }))
-  response_json["b_languagedata"] = language_b_data.map((item, index) => ({ ...item, number: index + 1 }))
-  response_json["categories"] = final_categories
-  return response_json
-
-
-  }else{
-    return false
+    response_json["a_languagedata"] = language_a_data.map((item, index) => ({ ...item, number: index + 1 }))
+    response_json["b_languagedata"] = language_b_data.map((item, index) => ({ ...item, number: index + 1 }))
+    response_json["categories"] = final_categories
+    return response_json
   }
+
+  return response_json;
 }
