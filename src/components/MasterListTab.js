b5c3e8ef71a6a335decc9f981858838f771e6d10
xming13 <tanweiseng13@gmail.com>
2020-05-09 11:52:40 +0800

    refactor: extract code to SearchInput component

--- src/components/MasterListTab.js
+++ src/components/MasterListTab.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CategoryList from './CategoryList'
 import NoResult from './NoResult'
+import SearchInput from './SearchInput'
 import TranslationCard from './TranslationCard'
 
 const MasterListTab = ({ searchInput, setSearchInput, selectedCategory, setSelectedCategory, filteredLanguageData = [], showBookmarkList, toggleShowBookmarkList, toggleBookmarkItem, hasBookmarkList }) => {
@@ -31,43 +32,7 @@ const MasterListTab = ({ searchInput, setSearchInput, selectedCategory, setSelec
     <div className="container-fluid">
       <div className="row">
         <div className="col" style={{ display: 'flex' }}>
-          <div className="input-group" style={{ margin: '16px 0' }}>
-            <input
-              type="search"
-              className="form-control input-search"
-              placeholder="Search"
-              aria-label="Search"
-              aria-describedby="button-search"
-              onChange={(e) => setSearchInput(e.target.value)}
-              x-webkit-speech={"true"}
-              style={{
-                borderWidth: 3,
-                borderTopLeftRadius: 100,
-                borderBottomLeftRadius: 100,
-                borderColor: '#A3A3A3',
-              }}
-            />
-            <div className="input-group-append">
-              <button
-                className="btn btn-outline-secondary button-search"
-                type="button"
-                id="button-search"
-                style={{
-                  paddingTop: 0,
-                  paddingBottom: 0,
-                  paddingLeft: 10,
-                  paddingRight: 10,
-                  borderWidth: 3,
-                  borderLeftWidth: 0,
-                  borderTopRightRadius: 100,
-                  borderBottomRightRadius: 100,
-                  borderColor: '#A3A3A3',
-                }}
-              >
-                <i className="fa fa-search"></i>
-              </button>
-            </div>
-          </div>
+          <SearchInput id="search-masterlist" setSearchInput={setSearchInput} />
           <div onClick={toggleShowBookmarkList} style={{
             display: 'flex',
             marginLeft: 12,
---
+++ src/components/SearchInput/SearchInput.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './SearchInput.css';
+
+const SearchInput = ({ setSearchInput, id }) => {
+  return (
+    <div className="input-group" style={{ margin: '16px 0' }}>
+      <input
+        type="search"
+        className="form-control input-search"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby={id}
+        onChange={(e) => setSearchInput(e.target.value)}
+        x-webkit-speech={"true"}
+        style={{
+          borderWidth: 3,
+          borderTopLeftRadius: 100,
+          borderBottomLeftRadius: 100,
+          borderColor: '#A3A3A3',
+        }}
+      />
+      <div className="input-group-append">
+        <button
+          className="btn btn-outline-secondary button-search"
+          type="button"
+          id={id}
+          style={{
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingLeft: 10,
+            paddingRight: 10,
+            borderWidth: 3,
+            borderLeftWidth: 0,
+            borderTopRightRadius: 100,
+            borderBottomRightRadius: 100,
+            borderColor: '#A3A3A3',
+          }}
+        >
+          <i className="fa fa-search"></i>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchInput
